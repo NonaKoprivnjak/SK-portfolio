36ad6cb0e32e3301fa7aefd85085b8b7a8762f67
Simona Koprivnjak <simonakoprivnjak@gmail.com>
2026-01-22 19:09:26 +0100

    portfolio.present

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-// Smooth scroll for nav links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
     e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
   });
 });
+
